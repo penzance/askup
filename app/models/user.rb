7d63309445160361622ba59199f244a343434262
Yates, Elliott <elliott_yates@harvard.edu>
2015-01-15 13:11:29 -0500

    updates Devise config; removes option to retrieve password by email (no email server currently configured)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_many :questions
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, recoverable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   def full_name
     first_name + " " + last_name
