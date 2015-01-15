1962738f64081d1ee6096a4ba29cf9179c5a4976
Yates, Elliott <elliott_yates@harvard.edu>
2015-01-15 12:54:43 -0500

    fixes bug where first and last name were not persisted on sign up form if there was an error processing the sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -8,5 +11,9 @@ class ApplicationController < ActionController::Base
     @@analytics_logger ||= Logger.new("#{Rails.root}" + ENV["analytics_log_file"])
   end
 
+  protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name
+  end
 end
