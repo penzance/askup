b844f5a044b79fb50d925ceb9c1d6551422c4349
Katherine Loboda <katyarina@Katherines-MacBook-Air-2.local>
2015-04-06 18:14:22 -0400

    Working Modal

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -9,18 +9,34 @@ $(document).ready(function(q){
       $('.my-question').addClass('hidden');
       $('.other-question').removeClass('hidden');
     });
-
+  
+// Defines function that disables/enables submit button depending on if there is text in the answer box
+function enableSubmitAnswer() {
+  if ($('#answer_text').val()) {
+    $('#submit_answer').attr('disabled', false);
+  } else {
+    $('#submit_answer').attr('disabled', true); 
+  }
+}
 
 $('#question_display_Modal').on('show.bs.modal', function (event) {
+  
+  $("#submit_answer").show();
+  $('#answer_text').val(""); 
+  enableSubmitAnswer(); 
+  $("#answers").hide();
+
+
   var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('question') // Extract info from data-* attributes
+  var recipient = button.data('question') 
+  var first_answer = button.data('answer')// Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   
   // reset all the elements that I need to reset --> specifically submit
   var modal = $(this)
   modal.find('.modal-title').text(recipient)
-  modal.find('.modal-')
+  modal.find('.first-answer').text(first_answer)
 })
 
 
