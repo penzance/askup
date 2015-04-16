6658c80415075e648157f026d33c2066ba090e12
Katherine Loboda <katyarina@Katherines-MacBook-Air-2.local>
2015-04-16 13:21:34 -0400

    Finshed Modal with Pointer feature

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -19,23 +19,48 @@ function enableSubmitAnswer() {
   }
 }
 
+// Defines function that gives the user feedback once they state whether or not they have gotten the question right. 
+// This function also stores the user response in the development_analytics.log using an ajax call. 
+ function provide_feedback(is_correct, notice_text, qid) {
+   $.ajax({
+    url: window.location.pathname +  "/" + q_id + "/feedback",
+     type: "POST",
+        data: { correct: is_correct }
+    })
+    .done(function( data ) {
+      if ( console && console.log ) {
+        console.log( "Sample of data:", data.slice( 0, 100 ) );
+      }
+      $('#alert_text').text(notice_text);
+      $('.alert').slideDown();
+    });
+  };
+
+// Creates the javscript for the modal that allows it to be populated with the data recieved upon clicking the modal link
 $('#question_display_Modal').on('show.bs.modal', function (event) {
   
+  // Creates the initial view of the modal. The submit answer is shown as well as an empty text box. 
+  // The answer is initally hidden and so is the response + alert forms. 
   $("#submit_answer").show();
   $('#answer_text').val(""); 
   enableSubmitAnswer(); 
   $("#answers").hide();
+  $("#alert_text_container").hide();
 
+  var question_link = $(event.relatedTarget) // Button that triggered the modal. question_link is populated with the data from the click 
 
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('question') 
-  var first_answer = button.data('answer')// Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  // Creates variables with data that we will later use to populate certain parts of the modal 
+  var text_question = question_link.data('question') 
+  var first_answer = question_link.data('answer')  
   
-  // reset all the elements that I need to reset --> specifically submit
+  // Setting up the response buttons to have the correct q_id to send to the analytics.log and to also have the 
+  $('#respond-yes').data('q_id', question_link.data('q_id')); 
+  $('#respond-no').data('q_id', question_link.data('q_id'));  
+
+ 
+  // Searches the modal and populates the indicated classes with the data recieved when the modal was clicked
   var modal = $(this)
-  modal.find('.modal-title').text(recipient)
+  modal.find('.modal-title').text(text_question)
   modal.find('.first-answer').text(first_answer)
 })
 
@@ -49,11 +74,6 @@ function enableSubmitQuestion() {
   }
 }
 
-// Function is called as soon as page is loaded so that submit button can be disabled initially 
-enableSubmitQuestion(); 
-
-// runs each time the user presses a key in the #answer_text form field
-$('#question_text, #question_answers_attributes_0_text').keyup(enableSubmitQuestion);
 
 }); 
 
