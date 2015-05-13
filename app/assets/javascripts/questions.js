ddd93b9d06e1a45726cf1ec093881719dd9b90ec
Katherine Loboda <katyarina@Katherines-MacBook-Air-2.local>
2015-05-13 13:53:38 -0400

    PR Updates

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -47,11 +47,11 @@ $('#question_display_Modal').on('show.bs.modal', function (event) {
   $("#response, #answers, #alert_text_container").hide();
   enableSubmitAnswer();  
 
-  var question_link = $(event.relatedTarget) // element that triggered the modal. question_link is populated with the data from the click 
+  var question_link = $(event.relatedTarget); // element that triggered the modal. question_link is populated with the data from the click 
 
   // Creates variables with data that we will later use to populate certain parts of the modal 
-  var text_question = question_link.data('question') 
-  var first_answer = question_link.data('answer')  
+  var text_question = question_link.data('question');
+  var first_answer = question_link.data('answer');  
   
   // Setting up the response buttons to have the correct q_id to send to the analytics.log and to also trigger the right feedback form
   $('#respond-yes').data('q_id', question_link.data('q_id')); 
@@ -59,32 +59,29 @@ $('#question_display_Modal').on('show.bs.modal', function (event) {
 
  
   // Searches the modal and populates the indicated classes with the data recieved when the modal was clicked
-  var modal = $(this)
-  modal.find('.modal-title').text(text_question)
-  modal.find('.first-answer').text(first_answer)
+  var modal = $(this);
+  modal.find('.modal-title').text(text_question);
+  modal.find('.first-answer').text(first_answer);
 })
-
 
 // Creates the javscript for the Social modal that allows it to be populated with the data recieved upon clicking the modal link
 $('#social_display_Modal').on('show.bs.modal', function (event) {
-  var social_link = $(event.relatedTarget) // element that triggered the modal. social_link is populated with the data from the click 
+  var social_link = $(event.relatedTarget); // element that triggered the modal. social_link is populated with the data from the click 
   
   // Creates variables with data that we will later use to populate certain parts of the modal 
-  var title = social_link.data('website')
-  var text_question = social_link.data('question')
-  var url_link = social_link.data('url')
-  var hyperlink = social_link.data('hyperlink')
-  var hyperlinktext = social_link.data('hyperlinktext')
+  var title = social_link.data('website');
+  var text_question = social_link.data('question');
+  var url_link = social_link.data('url');
+  var hyperlink = social_link.data('hyperlink');
+  var hyperlinktext = social_link.data('hyperlinktext');
 
   // Searches the modal and populates the indicated classes with the data recieved when the modal was clicked
-  var modal = $(this)
-  modal.find('.modal-title').text(title) // sets the title 
-  modal.find('.modal-body input').val(text_question + " ➡" + "Follow the link to find out the solution: " + url_link) // creates the text that includes the question and url for user to copy to clipboard
-  modal.find('.hyperlink').attr("href", hyperlink) // updates the href to the appropriate social platform
-  modal.find('.hyperlink').text(hyperlinktext) // creates the text that the hyperlink will show up as 
+  var modal = $(this);
+  modal.find('.modal-title').text(title); // sets the title 
+  modal.find('.modal-body-social input').val(text_question + " ➡" + "Follow the link to find out the answer: " + url_link); // creates the text that includes the question and url for user to copy to clipboard
+  modal.find('.hyperlink').attr("href", hyperlink).attr("target", "_blank"); // updates the href to the appropriate social platform the second attribute allows the social site to be opened on a new page 
+  modal.find('.hyperlink').text(hyperlinktext); // creates the text that the hyperlink will show up as 
 })
-
-
 
 // Defines function that disables/enables submit button depending on if there is text in the question and answer boxes
 function enableSubmitQuestion() {
@@ -95,16 +92,11 @@ function enableSubmitQuestion() {
   }
 }
 
-// allows the function to be enabled on the generate-question page 
+// enables/disables the submit question button on page load
 enableSubmitQuestion(); 
 
 // runs each time the user presses a key in the #answer_text form field
 $('#question_text, #question_answers_attributes_0_text').keyup(enableSubmitQuestion);
-
-
-
-
-
 
 }); 
 
