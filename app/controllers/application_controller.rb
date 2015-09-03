049c8b6bf9d4a218de8a65510b16bfd4a86b27e1
Yates, Elliott <elliott_yates@harvard.edu>
2015-09-03 14:48:33 -0400

    removes unnecessary helper files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  include ApplicationHelper
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Prevent CSRF attacks by raising an exception.
