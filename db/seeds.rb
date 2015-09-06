d8df7756dad451d8ddc5bb59c913f9042207014c
Yates, Elliott <elliott_yates@harvard.edu>
2015-09-05 23:32:14 -0400

    converts oldschool bootstrap rake task into seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Add the default admin user
+User.create(
+  first_name: 'Ask',
+  last_name: 'Up',
+  email: 'auadmin@example.com',
+  password: 'password',
+  role: 'admin'
+)
+
+# Add the default test user
+User.create(
+  first_name: 'Inquisitive',
+  last_name: 'Queryhands',
+  email: 'testuser@example.com',
+  password: 'password',
+  role: 'contributor'
+)
+
+# Create the root Qset
+Qset.create( name: 'Root' )
