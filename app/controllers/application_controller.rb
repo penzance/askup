bce0dbd4afdb0e082b9b4fc066a614ef14371701
Raymond Kim <rckim77@gmail.com>
2015-10-05 14:38:39 -0400

    analyzer logs at info level, app log logs at debug level in dev and error level in prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def analyzer
-    @@analytics_logger ||= Logger.new(Rails.configuration.askup.analytics.log_file)
+    if !defined?(@@analytics_logger)
+      @@analytics_logger = Logger.new(Rails.configuration.askup.analytics.log_file)
+      # set logger to level of info
+      @@analytics_logger.level = Logger::INFO 
+    end
+
+    @@analytics_logger
   end
 
   protected
