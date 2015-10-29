e92e60a9565378534fa03e0552afb8c0023b5cca
Katherine Loboda <katyarina@Katherines-MacBook-Air-2.local>
2015-10-29 18:01:37 -0400

    moved org_id devise parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
 
   protected 
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :organization
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) << :org_id
+  # end
 
   load_and_authorize_resource
 end
