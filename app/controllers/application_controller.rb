805010af9659df44207df460c9c2dda515157299
Katherine Loboda <katyarina@Katherines-MacBook-Air-2.local>
2015-10-29 22:11:46 -0400

    new idea for sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name << :org_id
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ org_id: [] }, :first_name, :last_name, :email, :password, :password_confirmation)}
   end
 
   rescue_from CanCan::AccessDenied do |exception|
