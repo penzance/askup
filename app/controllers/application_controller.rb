baa8e40c2768a4f7e18c54b234a2d337f42c74f6
Katherine Loboda <katyarina@Katherines-MacBook-Air-2.local>
2015-10-30 16:28:16 -0400

    fixed sanitizer that allows org_id to be stored in users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ org_id: [] }, :first_name, :last_name, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:sign_up) << :org_id << :first_name << :last_name
   end
 
   rescue_from CanCan::AccessDenied do |exception|
